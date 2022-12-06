ad2872541af7a2b1793d0673e3587ce751bffaf0
ilef dimassi <dimassiilef51@gmail.com>
2022-12-06 10:24:47 +0100

    checkpoint_state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fullName: "ilefDimassi",
+      bio: "i study the training of the full stack java script",
+      profesion: "student",
+      show: true,
+      src: "./fleur.webp",
+      count:0,
+    };
+  }
+  btn = () => {
+    this.setState({ show: !this.state.show ,count:0});
+  };
+  componentDidMount(){  setInterval( () => {
+    this.setState({ count: (this.state.count+1) });
+  }, 1000);}
+
+ 
+
+
+  render() {
+    return (
+      <div>
+       { this.state.show?(
+      <div className="profil">
+        <h1 className="name">{this.state.fullName}</h1>
+        {this.state.count}
+        <h3>{this.state.bio}</h3>
+        <h3>{this.state.profesion}</h3>
+        <img className="img" src={this.state.src}/>
+        <button  className="btn" onClick ={this.btn}>
+          showTrue
+        </button>
+      </div>):(<button className="btn" onClick ={this.btn}>
+          showTrue
+        </button>)}
+      </div>
+    );
+  }
 }
 
 export default App;
